56774914082e3d046bdcfa0c8f477af32c9775ed
Evgeny <direct.000@yandex.ru>
2020-05-17 23:21:57 +0300

    create project

---
+++ Quarantine/Quarantine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quarantine
+//
+//  Created by Lipadat Evgeniy on 17.05.2020.
+//  Copyright © 2020 Lipadat Evgeniy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
